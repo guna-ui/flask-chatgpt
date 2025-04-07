cba0784ad25b2821d4a60337c01278dece8e1cfc
gunako <gunasekhar449@gmail.com>
2025-04-07 20:15:44 +0530

    day 2- getproducts is done

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to ShopCartX API!"
+
+@app.route('/api/products',methods=['GET'])
+def getproducts():
+    products = [
+        {"id": 1, "name": "T-shirt", "price": 499},
+        {"id": 2, "name": "Jeans", "price": 999},
+        {"id": 3, "name": "Sneakers", "price": 1999}
+    ]
+    return jsonify(products)
 
 if __name__=='__main__':
     app.run(debug=True)
